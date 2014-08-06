9fb532176ea2680ff85b6c79454608236f7bde18
duyang <duyang@jpush.cn>
2014-08-06 20:10:54 +0800

    delete some file

--- src/test/AbstractTest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public abstract class AbstractTest implements MyInterface{
-	public AbstractTest(){
-		System.out.println("abstract test");
-	}
-}
--- src/test/MyInterface.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public interface MyInterface {
-	public void fuck();
-}
